27656d3ad090ae5ec2905430fc69735f2fb69b06
L1ndenbaum <zhangjianhao2986@gmail.com>
2025-11-25 14:10:53 +0800

    Modify a script

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -1,6 +1,6 @@
-sudo pacman -S yay waybar wofi wlogout waypaper libnotify mako dolpin \
+sudo pacman -S --needed yay waybar wofi wlogout waypaper libnotify mako dolpin \
 	qt5-wayland qt6-wayland fcitx5 fcitx5-rime \
 	hyprpolkitagent hypridle hyprlock hyprpaper \
 	networkmanager network-manager-applet blueman bluez pipewire wireplumber pipewire-pulse pipewire-alsa pipewire-jack \
 	wqy-zenhei awesome-terminal-fonts
-yay -S clash-verge-rev visual-studio-code-bin ttf-font-awesome-5
+yay -S --needed clash-verge-rev-bin visual-studio-code-bin ttf-font-awesome-5
